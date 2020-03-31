b520bdb9e24714840421c46e4bc8167649dd510b
Travis Frels <travis.frels@gmail.com>
2020-03-31 18:41:16 -0500

    Component Descriptor (#6)
    * added the component descriptor model; refactored byte helpers
    
    * role and mask constants in models are now public
    
    * refactored bitwise byte helpers
    
    * improve component descriptor property naming

---
+++ WellLog.Lib/Helpers/ByteHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WellLog.Lib.Helpers
+{
+    public static class ByteHelpers
+    {
+        public static bool GetBitUsingMask(this byte b, byte mask)
+        {
+            return (b & mask) > 0;
+        }
+
+        public static byte SetBitUsingMask(this byte b, byte mask)
+        {
+            return Convert.ToByte(b | mask);
+        }
+
+        public static byte ClearBitUsingMask(this byte b, byte mask)
+        {
+            return Convert.ToByte(b & ~mask);
+        }
+
+        public static byte AssignBitUsingMask(this byte b, byte mask, bool value)
+        {
+            return value ? b.SetBitUsingMask(mask) : b.ClearBitUsingMask(mask);
+        }
+
+        public static bool IsComponentRole(this byte b, byte role)
+        {
+            return (b >> 5) == (role >> 5);
+        }
+    }
+}
---
+++ WellLog.Lib/Models/DLIS/ComponentDescriptor.cs
@@ -0,0 +1,50 @@
+﻿using WellLog.Lib.Helpers;
+
+namespace WellLog.Lib.Models.DLIS
+{
+    public class ComponentDescriptor
+    {
+        public const byte ABSENT_ATTRIBUTE_ROLE = 0b_0000_0000;
+        public const byte ATTRIBUTE_ROLE = 0b_0010_0000;
+        public const byte INVARIANT_ATTRIBUTE_ROLE = 0b_0100_0000;
+        public const byte OBJECT_ROLE = 0b_0110_0000;
+        public const byte REDUNDANT_SET_ROLE = 0b_1010_0000;
+        public const byte REPLACEMENT_SET_ROLE = 0b_1100_0000;
+        public const byte SET_ROLE = 0b_1110_0000;
+
+        public const byte SET_TYPE_MASK = 0b_0001_0000;
+        public const byte SET_NAME_MASK = 0b_0000_1000;
+
+        public const byte OBJ_NAME_MASK = 0b_0001_0000;
+
+        public const byte ATTR_LABEL_MASK = 0b_0001_0000;
+        public const byte ATTR_COUNT_MASK = 0b_0001_0000;
+        public const byte ATTR_REPRESENTATION_CODE_MASK = 0b_0001_0000;
+        public const byte ATTR_UNITS_MASK = 0b_0001_0000;
+        public const byte ATTR_VALUE_MASK = 0b_0001_0000;
+
+        private readonly byte _descriptor;
+
+        public bool IsAbsentAttribute => _descriptor.IsComponentRole(ABSENT_ATTRIBUTE_ROLE);
+        public bool IsAttribute => _descriptor.IsComponentRole(ATTRIBUTE_ROLE);
+        public bool IsInvariantAttribute => _descriptor.IsComponentRole(INVARIANT_ATTRIBUTE_ROLE);
+        public bool IsObject => _descriptor.IsComponentRole(OBJECT_ROLE);
+        public bool IsRedundantSet => _descriptor.IsComponentRole(REDUNDANT_SET_ROLE);
+        public bool IsReplacementSet => _descriptor.IsComponentRole(REPLACEMENT_SET_ROLE);
+        public bool IsSet => _descriptor.IsComponentRole(SET_ROLE);
+
+        public bool DoesSetHaveType => (IsSet || IsRedundantSet || IsReplacementSet) ? _descriptor.GetBitUsingMask(SET_TYPE_MASK) : false;
+        public bool DoesSetHaveName => (IsSet || IsRedundantSet || IsReplacementSet) ? _descriptor.GetBitUsingMask(SET_NAME_MASK) : false;
+        public bool DoesObjectHaveName => IsObject ? _descriptor.GetBitUsingMask(OBJ_NAME_MASK) : false;
+        public bool DoesAttributeHaveLabel => (IsAttribute || IsInvariantAttribute) ? _descriptor.GetBitUsingMask(ATTR_LABEL_MASK) : false;
+        public bool DoesAttributeHaveCount => (IsAttribute || IsInvariantAttribute) ? _descriptor.GetBitUsingMask(ATTR_COUNT_MASK) : false;
+        public bool DoesAttributeHaveRepresentationCode => (IsAttribute || IsInvariantAttribute) ? _descriptor.GetBitUsingMask(ATTR_REPRESENTATION_CODE_MASK) : false;
+        public bool DoesAttributeHaveUnits => (IsAttribute || IsInvariantAttribute) ? _descriptor.GetBitUsingMask(ATTR_UNITS_MASK) : false;
+        public bool DoesAttributeHaveValue => (IsAttribute || IsInvariantAttribute) ? _descriptor.GetBitUsingMask(ATTR_VALUE_MASK) : false;
+
+        public ComponentDescriptor(byte descriptor)
+        {
+            _descriptor = descriptor;
+        }
+    }
+}
